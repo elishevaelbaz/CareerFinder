63bd557691eb86fae432de0cdc1630c531c56293
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2018-01-20 23:03:20 -0500

    added careers and pictures

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,38 +1,149 @@
 var friendList = [
-  {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  },
+    {
+      "name": "Accountant",
+      "photo": "images/accountant.jpg",
+      "scores":[
+          2,
+          1,
+          1,
+          5,
+          5,
+          1,
+          2,
+          1,
+          2,
+          5
+        ]
+    },
 
+    {
+      "name": "Teacher",
+      "photo": "images/teacher.jpg",
+      "scores":[
+      3,2,4,3,3,3,5,2,5,3
+          
+        ]
+    },
 
-   {
-    "name":"Bob",
-    "photo":"images/asian-girl.png",
-    "scores":[
-        1,
-        2,
-        3,
-        1,
-        1,
-        3,
-        5,
-        2,
-        2,
-        5
-      ]
-  }
+    {
+      "name": "Doctor/Paramedic",
+      "photo": "images/doctor.jpg",
+      "scores":[
+         5,3,2,4,5,5,4,4,3,5
+        ]
+    },
+
+    {
+      "name": "Chef",
+      "photo": "images/chef.jpg",
+      "scores":[
+          1,5,5,3,1,1,1,3,1,1
+        ]
+    },
+
+    {
+      "name": "Landscaper/Gardener",
+      "photo": "images/farmer.jpg",
+      "scores":[
+          2,5,4,4,2,1,1,1,1,1
+        ]
+    },
+
+    {
+      "name": "Pharmacist",
+      "photo": "images/pharmacist.jpg",
+      "scores":[
+          4,2,1,5,5,5,2,1,3,3
+        ]
+    },
+
+    {
+      "name": "Social Worker",
+      "photo": "images/socialWorker.jpg",
+      "scores":[
+          5,1,2,3,2,1,5,1,4,5
+        ]
+    },
+
+    {
+      "name": "Police Officer",
+      "photo": "images/police.jpg",
+      "scores":[
+          5,3,1,3,5,1,3,5,2,4
+        ]
+    },
+
+    {
+      "name": "Lawyer",
+      "photo": "images/lawyer.jpg",
+      "scores":[
+          4,1,1,5,4,1,3,1,5,3
+        ]
+    },
+
+    {
+      "name": "IT Professional",
+      "photo": "images/it.jpg",
+      "scores":[
+          2,1,3,4,2,2,1,1,2,5
+        ]
+    },
+
+    {
+      "name": "Photographer/Videographer",
+      "photo": "images/videographer.jpg",
+      "scores":[
+          1,2,4,2,1,1,2,1,2,1
+        ]
+    },
+
+    {
+      "name": "Pilot",
+      "photo": "images/pilot.jpg",
+      "scores":[
+          2,1,1,4,5,3,1,4,2,3
+        ]
+    },
+
+    {
+      "name": "Hairdresser",
+      "photo": "images/hairdresser.jpg",
+      "scores":[
+          1,5,5,1,1,1,4,1,3,1
+        ]
+    },
+
+    {
+      "name": "Electrician",
+      "photo": "images/electrician.jpg",
+      "scores":[
+          2,5,2,3,2,1,1,1,1,4
+        ]
+    },
+
+    {
+      "name": "Librarian",
+      "photo": "images/librarian.jpg",
+      "scores":[
+          1,1,2,4,4,1,3,1,5,3
+        ]
+    },
+
+    {
+      "name": "Veterinarian",
+      "photo": "images/vet.jpg",
+      "scores":[
+          4,3,1,3,4,4,2,2,1,4
+        ]
+    },
+
+    {
+      "name": "Nanny",
+      "photo": "images/nanny.jpg",
+      "scores":[
+          3,1,3,1,1,1,5,1,3,2
+        ]
+    }
 
 ]
 
