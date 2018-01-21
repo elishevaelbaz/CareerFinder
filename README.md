44fa690f11b7075625be0926467c23fcc4e1e1fd
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2018-01-21 00:43:43 -0500

    adding default - catch all to land at home pageA

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# DessertFinder
+# Career Finder
+
+### Overview
+
+This app is a career finder. This full-stack site takes in results from the users' surveys, and compares their answers with those for different professiosn. The app then displays the name and picture of the career with the best overall match. 
+
+The app uses Express to handle routing. The app is deployed on Heroku.
+
+### Node Packages
+ This app uses path, express and body-parser node packages (see package.json).
+
+
+### Instructions
+
+1. The survey has 10 questions. Each answer is on a scale of 1 to 5 based on how much the user agrees or disagrees with the question.
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,12 +3,25 @@ var path = require("path");
 
 module.exports = function (app){
 
-	app.get("/survey", function(req, res) {
-	  res.sendFile(path.join(__dirname, "../public/survey.html"));
-	});
 
-	app.get("/", function(req, res) {
-	  res.sendFile(path.join(__dirname, "./../public/home.html"));
+
+	app.get("/:input?", function(req, res) {
+
+		var input = req.params.input;
+
+		switch (input){
+
+			case "survey":
+				res.sendFile(path.join(__dirname, "../public/survey.html"));
+				break;
+
+			// default/catch all route - leads to home page
+			default:
+				res.sendFile(path.join(__dirname, "./../public/home.html"));
+				break;
+
+		}
+	  
 	});
 
 }
